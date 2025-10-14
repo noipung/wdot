0476116947779b9cc4fcbd88ecd1688a4cc0cead
noipung <noipung001@gmail.com>
2025-10-14 13:34:13 +0900

    Implement GA4 tracking

--- src/main.js
+++ src/main.js
@@ -3,7 +3,29 @@ import { initEventListeners } from "../modules/events.js";
 import { initPaletteUI } from "../modules/palette.js";
 import "../modules/ui.js";
 
-const initializeApp = async () => {
+const initGaAnalytics = () => {
+  if (!import.meta.env.PROD) return;
+
+  const s1 = document.createElement("script");
+  s1.async = true;
+  s1.src = `https://www.googletagmanager.com/gtag/js?id=${
+    import.meta.env.VITE_GA_ID
+  }`;
+  document.head.appendChild(s1);
+
+  const s2 = document.createElement("script");
+  s2.innerHTML = `
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', '${import.meta.env.VITE_GA_ID}');
+  `;
+  document.head.appendChild(s2);
+};
+
+const initApp = async () => {
+  initGaAnalytics();
+
   try {
     await initPaletteUI();
     initEventListeners();
@@ -12,4 +34,4 @@ const initializeApp = async () => {
   }
 };
 
-initializeApp();
+initApp();
